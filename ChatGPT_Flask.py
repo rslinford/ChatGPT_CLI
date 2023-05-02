14089a65486c82abf2b6cc9acddc5e825d6d01a3
Scott <scott@linfords.com>
2023-05-02 01:22:28 -0700

    Added top_p

--- ChatGPT_Flask.py
+++ ChatGPT_Flask.py
@@ -1,24 +1,25 @@
-from flask import Flask, render_template, request
 import openai
+from flask import Flask, render_template, request
 
-
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Configure OpenAI API key
 with open("api_key.txt", "r") as file:
     openai.api_key = file.read().strip()
 
-# Define the home page
+
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html')
+    return render_template('home.html',
+                           temperature=1,
+                           top_p=1)
+
 
 @app.route('/', methods=['POST'])
 def home_post():
     user_prompt = request.form['user_prompt']
     system_message = request.form['system_message']
     temperature = request.form['temperature']
+    top_p = request.form['top_p']
 
     messages = [
         {"role": "system", "content": system_message},
@@ -26,25 +27,25 @@ def home_post():
     ]
 
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = messages,
-        temperature = float(temperature),
-        max_tokens = 1024)
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=float(temperature),
+        top_p=float(top_p),
+        max_tokens=1024)
 
     assistant_response = response['choices'][0]['message']['content']
     finish_reason = response['choices'][0]['finish_reason']
     # todo: store messages in session
     messages.append({"role": "assistant", "content": assistant_response})
 
-    # Render the response template with the assistant response
     return render_template('home.html',
                            user_prompt=user_prompt,
                            system_message=system_message,
                            assistant_response=assistant_response,
                            finish_reason=finish_reason,
-                           temperature=temperature)
+                           temperature=temperature,
+                           top_p=top_p)
 
 
-# Run the application
 if __name__ == '__main__':
     app.run(debug=True)
